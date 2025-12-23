38ad1b168c4716ae42126041a7ba3761aeb76147
Mallevaey Lino <mallevaey.lino@coodlab.fr>
2025-12-23 22:26:52 +0100

    config: Update Vite `base` path to `/oratio/` for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Si vous déployez sur https://<user>.github.io/<repo>/, 
-  // remplacez './' par '/<repo>/' (ex: '/oratio/')
-  base: './', 
+  base: '/oratio/',
 })
